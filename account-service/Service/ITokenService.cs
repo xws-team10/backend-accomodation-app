3a49751d048bc28a0317f4b8d350a911bde1d291
markovicuros <101749196+markovicuros@users.noreply.github.com>
2023-05-13 14:06:28 +0200

    added account service

---
+++ account-service/Service/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlyMateAPI.Core.Model;
+
+namespace API.Core.Service.Core
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateToken(User user);
+    }
+}
