910b005817d05b7f5682936e5ff2e5c4638e3f36
markovicuros <101749196+markovicuros@users.noreply.github.com>
2023-05-14 18:04:19 +0200

    fixed user features

--- account-service/Model/DTO/UserDto.cs
+++ account-service/Model/DTO/UserDto.cs
@@ -2,8 +2,13 @@
 {
     public class UserDto
     {
+        public Guid Id { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
         public string UserRole { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
     }
 }
