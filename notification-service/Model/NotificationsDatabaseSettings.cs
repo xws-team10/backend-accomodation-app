567e80f00ce84e945a32f18b4371e3e33e0e5d20
DaniloBulatovic <93370954+DaniloBulatovic@users.noreply.github.com>
2023-06-16 21:22:06 +0200

    Added notification user settings

--- notification-service/Model/NotificationsDatabaseSettings.cs
+++ notification-service/Model/NotificationsDatabaseSettings.cs
@@ -5,5 +5,6 @@
         public string ConnectionString { get; set; } = null!;
         public string DatabaseName { get; set; } = null!;
         public string NotificationsCollectionName { get; set; } = null!;
+        public string NotificationUserSettingsCollectionName { get; set; } = null!;
     }
 }
--- notification-service/Service/NotificationService.cs
+++ notification-service/Service/NotificationService.cs
@@ -7,10 +7,12 @@ namespace notification_service.Service
     public class NotificationService : INotificationService
     {
         private readonly NotificationRepository _repository;
+        private readonly NotificationUserSettingsService _notificationUserSettingsService;
 
-        public NotificationService(NotificationRepository repository)
+        public NotificationService(NotificationRepository repository, NotificationUserSettingsService notificationUserSettingsService)
         {
             _repository = repository;
+            _notificationUserSettingsService = notificationUserSettingsService;
         }
 
         public async Task<List<Notification>> GetAllAsync() =>
@@ -19,8 +21,28 @@ namespace notification_service.Service
         public async Task<Notification> GetByIdAsync(Guid id) =>
             await _repository.GetByIdAsync(id);
 
-        public async Task<List<Notification>> GetAllByUserAsync(Guid id) =>
-            await _repository.GetAllByUserAsync(id);
+        public async Task<List<Notification>> GetAllByUserAsync(Guid id)
+        {
+            NotificationUserSettings userSettings = await _notificationUserSettingsService.GetByUserAsync(id);
+            List<NotificationType> types = new List<NotificationType>();
+            if (userSettings.showReservationRequestCreated)
+                types.Add(NotificationType.RESERVATION_REQUEST_CREATED);
+            if (userSettings.showReservationCanceled)
+                types.Add(NotificationType.RESERVATION_CANCELED);
+            if (userSettings.showHostGraded)
+                types.Add(NotificationType.HOST_GRADED);
+            if (userSettings.showAccomodationGraded)
+                types.Add(NotificationType.ACCOMODATION_GRADED);
+            if (userSettings.showSuperHost)
+                types.Add(NotificationType.SUPER_HOST);
+            if (userSettings.showReservationRequestReply)
+                types.Add(NotificationType.RESERVATION_REQUEST_REPLY);
+
+            List<Notification> allNotifications = await _repository.GetAllByUserAsync(id);
+            List<Notification> filteredNotifications = (List<Notification>)allNotifications.Where(n => types.Contains(n.Type));
+
+            return filteredNotifications;
+        }
 
         public async Task<List<Notification>> GetUnreadByUserAsync(Guid id) =>
             await _repository.GetUnreadByUserAsync(id);
